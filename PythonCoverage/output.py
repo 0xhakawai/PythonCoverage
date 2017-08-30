de0e15b60b21b9f7867302bbd1b08498306fd93a
Sa Vo <vtkimsa@gmail.com>
2017-08-30 16:45:41 +0700

    initial source

---
+++ PythonCoverage/output.py
@@ -0,0 +1,27 @@
+# Assign your variables below, each on its own line!
+caesar = "Graham"
+
+praline = "John"
+
+viking = "Teresa"
+
+# Put your variables above this line
+
+print caesar
+print praline
+print viking
+
+parrot = "Norwegian Blue"
+print len(parrot)
+
+print parrot.lower()
+print parrot.upper()
+
+pi = 3.14
+
+print str(pi)
+
+ministry = "The Ministry of Silly Walks"
+
+print len(ministry)
+print ministry.upper()
