6ce2e77beec8c7f093026b2f0f8ce9269a7fd809
Sa Vo <vtkimsa@gmail.com>
2017-08-30 17:52:36 +0700

    remove all files

--- PythonCoverage/outputdate.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-now = datetime.now()
-print now.year
-print now.month
-print now.day
-
-print '%s/%s/%s' % (now.month, now.day, now.year)
-print '%s:%s:%s' % (now.hour, now.minute, now.second)
-print '%s/%s/%s %s:%s:%s' % (now.month, now.day, now.year, now.hour, now.minute, now.second)
