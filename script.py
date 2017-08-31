de69535fd5f2af2fad301ee68389e48ad5c5f627
Sa Vo <vtkimsa@gmail.com>
2017-08-31 11:41:39 +0700

    update script.py

--- script.py
+++ script.py
@@ -1,31 +1,40 @@
-print "Welcome to Python!"
-# Write your code below!
-my_variable = 10
+import sys
 
-# Set the variables to the values listed in the instructions!
+def hello():
 
-my_int = 7
-my_float = 1.23
-my_bool = True
+    print "Welcome to Python!"
+    # Write your code below!
+    my_variable = 10
 
-# my_int is set to 7 below. What do you think
-# will happen if we reset it to 3 and print the result?
+    # Set the variables to the values listed in the instructions!
 
-my_int = 7
+    my_int = 7
+    my_float = 1.23
+    my_bool = True
 
-# Change the value of my_int to 3 on line 8!
+    # my_int is set to 7 below. What do you think
+    # will happen if we reset it to 3 and print the result?
 
-my_int = 3
+    my_int = 7
 
-# Here's some code that will print my_int to the console:
-# The print keyword will be covered in detail soon!
+    # Change the value of my_int to 3 on line 8!
 
-print my_int
+    my_int = 3
+
+    # Here's some code that will print my_int to the console:
+    # The print keyword will be covered in detail soon!
+
+    print my_int
 
 
 def spam():
     eggs = 12
     return eggs
 
+def main(n):
+    res = fact(n)
+    print(res)
 
-print spam()
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        main(int(sys.argv[1]))
